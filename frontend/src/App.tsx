b2077ec91a8640c77d362e334f1e1d0bf8868c1b
tiffany <tiffany.gomez@epitech.eu>
2025-01-10 12:28:44 +0100

    Accueil page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import './App.css';
 
 const App: React.FC = () => {
+
+  const [activeTab, setActiveTab] = useState<'connexion' | 'inscription'>('connexion');
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <div className="image-container">
+        <img
+          className="cat-picture"
+          src="/cat-picture.jpg"
+          alt="cat"
+        />
+      </div>
+      <div className="connexion-container">
+      <div className="tab-container">
+          <button
+            className={`tab ${activeTab === 'connexion' ? 'active' : ''}`}
+            onClick={() => setActiveTab('connexion')}
+          >
+            Connexion    
+          </button>
+          <button
+            className={`tab ${activeTab === 'inscription' ? 'active' : ''}`}
+            onClick={() => setActiveTab('inscription')}
+          >
+            Inscription
+          </button>
+        </div>
+
+        <form className="form">
+          {activeTab === 'connexion' && (
+            <>
+              <label htmlFor="nickname">Pseudo</label>
+              <input type="text" id="nickname" placeholder="Entrez votre pseudo" />
+            </>
+          )}
+
+          {activeTab === 'inscription' && (
+            <>
+              <label htmlFor="nickname">Pseudo</label>
+              <input type="text" id="nickname" placeholder="Entrez votre pseudo" />
+              <label htmlFor="password">Mot de passe</label>
+              <input type="password" id="password" placeholder="Entrez votre mot de passe" />
+            </>
+          )}
+        </form>
+              </div>
+    </>
   );
 };
 
