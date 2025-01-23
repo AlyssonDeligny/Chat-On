179ecfa32afaf7cc7e7f6b6093fba5a334c7bf8a
Alysson <alysson.deligny@gmail.com>
2025-01-23 11:21:07 +0100

    fix(vite): update paths to correctly serve assets

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
     // await app.listen(process.env.PORT ?? 3000);
+    app.enableCors({
+      origin: 'http://localhost:5173', // Autorise les requêtes du front (Vite)
+      credentials: true
+    });
+
     const PORT = 3000;
     await app.listen(PORT);
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
